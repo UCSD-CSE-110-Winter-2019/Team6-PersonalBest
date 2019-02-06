0422f3e34c0967aad89d9d8d97f709663b684d0e
yaffoman <yaffoman@gmail.com>
2019-02-05 16:43:57 -0800

    google login set up and height popup with save set

--- app/src/main/java/com/example/personalbest/MainActivity.java
+++ app/src/main/java/com/example/personalbest/MainActivity.java
@@ -1,44 +1,40 @@
 package com.example.personalbest;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.personalbest.fitness.FitnessService;
+import com.example.personalbest.fitness.FitnessServiceFactory;
+import com.example.personalbest.fitness.GoogleFitAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private String fitnessServiceKey = "GOOGLE_FIT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(StepCountActivity stepCountActivity) {
+                return new GoogleFitAdapter(stepCountActivity);
+            }
+        });
+
+        launchStepCountActivity();
     }
 
+    public void launchStepCountActivity() {
+        Intent intent = new Intent(this, StepCountActivity.class);
+        intent.putExtra(StepCountActivity.FITNESS_SERVICE_KEY, fitnessServiceKey);
+        startActivity(intent);
+    }
+
+    public void setFitnessServiceKey(String fitnessServiceKey) {
+        this.fitnessServiceKey = fitnessServiceKey;
+    }
 }
---
+++ app/src/main/java/com/example/personalbest/StepCountActivity.java
@@ -0,0 +1,139 @@
+package com.example.personalbest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.personalbest.fitness.FitnessService;
+import com.example.personalbest.fitness.FitnessServiceFactory;
+import com.example.personalbest.fitness.HeightPickerFragment;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.DataReadRequest;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+public class StepCountActivity extends AppCompatActivity {
+
+    public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
+
+    private static final String TAG = "StepCountActivity";
+
+    private TextView textSteps;
+    private long numSteps;
+    private FitnessService fitnessService;
+    private EncourageTask runner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_count);
+        textSteps = findViewById(R.id.textSteps);
+
+        String fitnessServiceKey = getIntent().getStringExtra(FITNESS_SERVICE_KEY);
+        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
+        Button btnUpdateSteps = findViewById(R.id.buttonUpdateSteps);
+        btnUpdateSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fitnessService.updateStepCount();
+                runner = new EncourageTask();
+                runner.execute();
+            }
+        });
+
+        fitnessService.setup();
+
+        SharedPreferences myPrefs = getSharedPreferences("height", MODE_PRIVATE);
+
+        //Log.i( "TAG","hello+test " + myPrefs.getString("height_feet",""));
+        if(!myPrefs.contains("height_feet")) {
+            DialogFragment nameFrag = new HeightPickerFragment();
+            nameFrag.show(getSupportFragmentManager(), "Height");
+        }
+
+
+
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//       If authentication was required during google fit setup, this will be called after the user authenticates
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == fitnessService.getRequestCode()) {
+                fitnessService.updateStepCount();
+            }
+        } else {
+            Log.e(TAG, "ERROR, google fit result code: " + resultCode);
+        }
+    }
+
+    public void setStepCount(long stepCount) {
+        textSteps.setText(String.valueOf(stepCount));
+        numSteps = stepCount;
+    }
+
+    public void showEncouragement(){
+       // int x = Integer.parseInt(textSteps.getText().toString()) / 100;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast t = Toast.makeText(StepCountActivity.this,"Good job! You're already at " + (numSteps/100) + "% of the daily recommended number of steps.", Toast.LENGTH_LONG);
+                t.show();
+            }
+        });
+
+    }
+
+    private class EncourageTask extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            showEncouragement();
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+          /*  while (true) {
+                if (isCancelled()) {
+                    break;
+                }
+                if(numSteps >= 1000){
+                    publishProgress();
+                    break;
+                }
+            }*/
+
+            if(numSteps>=1000)
+                publishProgress();
+            return "ALL GOOD";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+    }
+}
