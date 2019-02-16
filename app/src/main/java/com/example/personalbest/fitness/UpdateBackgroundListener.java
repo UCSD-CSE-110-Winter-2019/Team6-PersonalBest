cf97dc797d3ba679544c46baff9187ba7246e730
alermi <alermi@ucsd.edu>
2019-02-15 17:21:21 -0800

    First start steps working

--- app/src/main/java/com/example/personalbest/fitness/UpdateBackgroundListener.java
+++ app/src/main/java/com/example/personalbest/fitness/UpdateBackgroundListener.java
@@ -1,16 +1,19 @@
 package com.example.personalbest.fitness;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.example.personalbest.SaveLocal;
 import com.example.personalbest.StepCountActivity;
 import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.Calendar;
 import java.util.Date;
 
-public class UpdateBackgroundListener implements OnSuccessListener<DataReadResponse> {
+public class UpdateBackgroundListener implements OnSuccessListener<DataReadResponse>, OnFailureListener{
     SaveLocal saveLocal;
     int daysBefore;
     Activity activity;
@@ -33,4 +36,8 @@ public class UpdateBackgroundListener implements OnSuccessListener<DataReadRespo
         //Update data
     }
 
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        Log.d("ONFAILURE","IT FAILED" );
+    }
 }
